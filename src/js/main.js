e3244b0abaf6725dfda2e6eb8074aafd8c69d75b
Alexey Kurpachyov <kurp96@ya.ru>
2019-08-05 22:35:24 +0300

    Написан скрипт модального окна с таймером 5 секунд

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+var button = document.querySelector('#button');
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close')
+
+button.addEventListener('click', function(){
+    modal.classList.add('modal_active');
+    setTimeout(() => modal.classList.remove('modal_active'),5000);
+});
+close.addEventListener('click', function(){
+    modal.classList.remove('modal_active');
+})
