37c24dddd601cb3d7eb9edf43fcb889b651bee9a
Alexey Kurpachyov <kurp96@ya.ru>
2019-08-07 20:35:41 +0300

    сделана якорная кнопка наверх

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
     close.on('click', function(){
         modal.removeClass('modal_active');
     });
+    // Подключим кнопку-якорь наверх
+    $('body').append('<button class="button-top">');
+    $('.button-top').on('click', function(){
+        $('body').animate({'scrollTop': 0}, 500);
+        $('html').animate({'scrollTop': 0}, 500); //разные браузеры работают по-разному
+    });
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 50){$('.button-top').addClass('button-top_active')}
+        else{$('.button-top').removeClass('button-top_active')}
+
+    });
 });
